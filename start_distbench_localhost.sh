2e7e32f0e7c56aac0d68679a18354042035061e7
Olivier Serres <oserres@google.com>
2021-06-07 02:16:09 +0000

    start_distbench_localhost.sh: Add option -d to run in debug mode under gdb

--- start_distbench_localhost.sh
+++ start_distbench_localhost.sh
@@ -45,6 +45,7 @@ OPTIND=1
 
 # Script parameters
 VERBOSE=0
+DEBUG=0
 DEFAULT_NODE_MANAGER_COUNT=1
 NODE_MANAGER_COUNT=$DEFAULT_NODE_MANAGER_COUNT
 
@@ -54,18 +55,21 @@ show_help() {
   echo
   echo "   -h               Display the usage help (this)"
   echo "   -v               Enable verbose mode"
+  echo "   -d               Enable debug mode"
   echo "   -n node_manager_cnt Specify the number of node manager to start"
   echo "                      default: $DEFAULT_NODE_MANAGER_COUNT"
   echo
 }
 
-while getopts "h?vn:" opt; do
+while getopts "h?vdn:" opt; do
     case "$opt" in
     h|\?)
         show_help
         exit 0
         ;;
     v)  VERBOSE=1
+        ;;
+    d)  DEBUG=1
         ;;
     n)  NODE_MANAGER_COUNT=$OPTARG
         ;;
@@ -83,22 +87,47 @@ if [[ "${VERBOSE}" = "1" ]]; then
   echo
 fi
 
+echo_and_run() {
+  if [[ "${VERBOSE}" = "1" ]]; then
+    echo
+    echo "\$ $*"
+  fi
+  "$@"
+}
+
+run_gdb_backtrace() {
+  echo Running "$*" under gdb
+  gdb -batch -ex "run" -ex "bt" --args "$@" 2>&1 | grep -v ^"No stack."$
+}
+
 check_dependencies
 build_distbench
 
 # Run a test_sequencer and node_manager instance
 #
 echo DistBench built, starting up an instance on localhost...
-set -x
-bazel run :distbench -c opt -- test_sequencer --port=10000 &
-sleep 3
-echo Nb of NODE_MANAGER_COUNT to start $NODE_MANAGER_COUNT
-for i in $(seq 1 1 $NODE_MANAGER_COUNT)
-do
-  bazel run :distbench -c opt -- node_manager --test_sequencer=localhost:10000 --port=$((9999-$i)) &
-done
-set +x
-sleep 5
+if [[ "${DEBUG}" = "1" ]]; then
+  echo_and_run bazel build --compilation_mode=dbg :all
+  run_gdb_backtrace bazel-bin/distbench test_sequencer --port=10000 &
+  sleep 3
+  run_gdb_backtrace
+  for i in $(seq 1 1 $NODE_MANAGER_COUNT)
+  do
+    run_gdb_backtrace bazel-bin/distbench node_manager --test_sequencer=localhost:10000 --port=$((9999-$i)) &
+  done
+  sleep 5
+else
+  echo Starting the Distbench test sequencer
+  echo_and_run bazel run :distbench -c opt -- test_sequencer --port=10000 &
+  sleep 3
+
+  echo Starting $NODE_MANAGER_COUNT Distbench node managers
+  for i in $(seq 1 1 $NODE_MANAGER_COUNT)
+  do
+    echo_and_run bazel run :distbench -c opt -- node_manager --test_sequencer=localhost:10000 --port=$((9999-$i)) &
+  done
+  sleep 5
+fi
 
 # Verify that Distbench is up and running
 #
