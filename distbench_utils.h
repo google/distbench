ca5f4fa7e384be7a5a517f558360cd0eadac4c3b
Dan Manjarres <danmanj@google.com>
2021-06-20 17:35:00 -0700

    Delete unused macros.

--- distbench_utils.h
+++ distbench_utils.h
@@ -65,30 +65,4 @@ grpc::Status abslStatusToGrpcStatus(const absl::Status &status);
 absl::Status grpcStatusToAbslStatus(const grpc::Status &status);
 }  // namespace distbench
 
-
-#if 0
-#define ASSIGN_OR_RETURN_IMPL(status, lhs, rexpr) \
-  absl::Status status = DoAssignOrReturn(lhs, (rexpr)); \
-      if (ABSL_PREDICT_FALSE(!status.ok())) return status;
-
-template<typename T>
-absl::Status DoAssignOrReturn(T& lhs, absl::StatusOr<T> result) {
-    if (result.ok()) {
-          lhs = result.ValueOrDie();
-            }
-      return result.status();
-}
-
-#define ASSIGN_OR_RETURN(lhs, rexpr) \
-  ASSIGN_OR_RETURN_IMPL( \
-      (_status_or_value), lhs, rexpr);
-
-#define RETURN_IF_ERROR(expr)                                                \
-  do {                                                                       \
-    /* Using _status below to avoid capture problems if expr is "status". */ \
-    const absl::Status _status = (expr);              \
-    if (ABSL_PREDICT_FALSE(!_status.ok())) return _status;               \
-  } while (0)
-#endif
-
 #endif  // DISTBENCH_DISTBENCH_UTILS_H_
