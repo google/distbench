3f702edd2c243fad33a8415026c3541f64d04a9d
Olivier Serres <oserres@google.com>
2021-07-09 04:35:16 +0000

    Add a run_tests option
    The run_tests option runs a test sequence from a proto text file,
    display the summary and optionnaly save the result to a file.
    
    Having this integrated allows us to remove the dependency on grpc_cli,
    and its 4MB limit.

--- distbench_busybox.cc
+++ distbench_busybox.cc
@@ -14,12 +14,20 @@
 
 #include "distbench_node_manager.h"
 #include "distbench_test_sequencer.h"
+
+#include <fcntl.h>
+
+#include <fstream>
+
 #include "absl/flags/flag.h"
 #include "absl/flags/parse.h"
+#include "google/protobuf/io/zero_copy_stream_impl.h"
+#include "google/protobuf/text_format.h"
 
 namespace {
 bool AreRemainingArgumentsOK(std::vector<char*> remaining_arguments,
                              size_t min_expected, size_t max_expected);
+int MainRunTests(std::vector<char*> &arguments);
 int MainTestSequencer(std::vector<char*> &arguments);
 int MainNodeManager(std::vector<char*> &arguments);
 void Usage();
@@ -49,6 +57,8 @@ int main(int argc, char** argv, char** envp) {
     return MainTestSequencer(remaining_arguments);
   } else if (!strcmp(distbench_module, "node_manager")) {
     return MainNodeManager(remaining_arguments);
+  } else if (!strcmp(distbench_module, "run_tests")) {
+    return MainRunTests(remaining_arguments);
   }
 
   std::cerr << "Unrecognized distbench module: " << distbench_module << "\n";
@@ -77,6 +87,104 @@ bool AreRemainingArgumentsOK(std::vector<char*> remaining_arguments,
   }
 
   return true;
+}
+
+absl::Status ParseTestSequenceProtoFromFile(
+    char *filename,
+    distbench::TestSequence *test_sequence) {
+  int fd_proto = STDIN_FILENO;
+  if (strcmp(filename, "-") != 0)
+    fd_proto = open(filename, O_RDONLY);
+  if (fd_proto < 0) {
+    std::string error_message{"Error opening the TestSequence proto file for "
+      "reading: "};
+    return absl::InvalidArgumentError(error_message + filename);
+  }
+
+  google::protobuf::io::FileInputStream fis_testproto(fd_proto);
+  if (fd_proto != STDIN_FILENO)
+    fis_testproto.SetCloseOnDelete(true);
+  if (!google::protobuf::TextFormat::Parse(&fis_testproto, test_sequence)) {
+    return absl::InvalidArgumentError(
+        "Error parsing the TestSequence proto file");
+  }
+
+  return absl::OkStatus();
+}
+
+absl::Status SaveResultProtoToFile(char *filename,
+                              const distbench::TestSequenceResults &result) {
+  int fd_proto = STDOUT_FILENO;
+  if (strcmp(filename, "-") != 0)
+    fd_proto = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+  if (fd_proto < 0) {
+    std::string error_message{"Error opening the output result proto file for "
+      "writing: "};
+    return absl::InvalidArgumentError(error_message + filename);
+  }
+
+  google::protobuf::io::FileOutputStream fos_resultproto(fd_proto);
+  if (fd_proto != STDOUT_FILENO)
+    fos_resultproto.SetCloseOnDelete(true);
+  if (!google::protobuf::TextFormat::Print(result, &fos_resultproto)) {
+    return absl::InvalidArgumentError(
+        "Error writing the result proto file");
+  }
+
+  return absl::OkStatus();
+}
+
+int MainRunTests(std::vector<char*> &arguments) {
+  // arguments: test_sequence.proto_text [result.proto_text]
+  if (!AreRemainingArgumentsOK(arguments, 1, 2))
+    return 1;
+
+  distbench::TestSequence test_sequence;
+  absl::Status parse_status = ParseTestSequenceProtoFromFile(arguments[0],
+                                                             &test_sequence);
+  if (!parse_status.ok())
+    return 1;
+
+  std::shared_ptr<grpc::ChannelCredentials> client_creds =
+      distbench::MakeChannelCredentials();
+  std::shared_ptr<grpc::Channel> channel = grpc::CreateCustomChannel(
+      absl::GetFlag(FLAGS_test_sequencer), client_creds,
+      distbench::DistbenchCustomChannelArguments());
+  std::unique_ptr<distbench::DistBenchTestSequencer::Stub> stub =
+    distbench::DistBenchTestSequencer::NewStub(channel);
+
+  if (!stub) {
+    std::cerr << "Error connecting to the TestSequencer\n";
+    return 1;
+  }
+
+  grpc::ClientContext context;
+  distbench::TestSequenceResults test_results;
+  grpc::Status status = stub->RunTestSequence(&context, test_sequence,
+                                              &test_results);
+  if (status.ok()) {
+    for (const auto& test_result: test_results.test_results()) {
+      std::cout << "Test summary:\n";
+      for (const auto& log_summary: test_result.log_summary()) {
+        std::cout << log_summary << "\n";
+      }
+      std::cout << "\n";
+    }
+  } else {
+    std::cerr << "Failed! " << status << "\n";
+  }
+
+  if (arguments.size() == 2) {
+    char *result_filename = arguments[1];
+    absl::Status save_status = SaveResultProtoToFile(result_filename,
+                                                     test_results);
+    if (!save_status.ok()) {
+      std::cerr << "Unable to save the results: " << save_status << "\n";
+      return 1;
+    }
+  }
+
+  return 0;
 }
 
 int MainTestSequencer(std::vector<char*> &arguments) {
@@ -124,6 +232,8 @@ void Usage() {
   std::cerr << "      --port=port_number    The port for the "
                                  "node_manager to listen on.\n";
 
+  std::cerr << "  distbench run_tests test_sequence.proto_text "
+                    "[result.proto_text] [--test_sequencer=host:port]\n";
   std::cerr << "\n";
   std::cerr << "For more options information, do\n";
   std::cerr << "  distbench --helpfull\n";
