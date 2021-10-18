cf439deb6420d5cc356f3c9aaa807911b72d1017
Olivier Serres <oserres@google.com>
2021-10-18 12:02:34 -0700

    kokoro: Install Bazel version 4.2.1 and GCC version 9

--- kokoro/gcp_ubuntu/kokoro_build.sh
+++ kokoro/gcp_ubuntu/kokoro_build.sh
@@ -15,6 +15,61 @@ set -e
 # Code under repo is checked out to ${KOKORO_ARTIFACTS_DIR}/github.
 # The final directory name in this path is determined by the scm name specified
 # in the job configuration.
+
+export BAZEL_VERSION=4.2.1
+
+function bazel_install {
+  echo
+  echo Downloading and installing Bazel version $BAZEL_VERSION
+  echo
+
+  rm -rf ~/bazel_install
+  mkdir ~/bazel_install
+  cd ~/bazel_install
+
+  wget https://github.com/bazelbuild/bazel/releases/download/"${BAZEL_VERSION}"/bazel-"${BAZEL_VERSION}"-installer-linux-x86_64.sh
+
+  chmod +x bazel-*.sh
+  ./bazel-"${BAZEL_VERSION}"-installer-linux-x86_64.sh --user
+  rm bazel-"${BAZEL_VERSION}"-installer-linux-x86_64.sh
+  cd ~/
+
+  # Remove the cache has sometime they conflict between versions
+  rm -rf  ~/.cache/bazel
+}
+
+
+function update_gcc() {
+  echo
+  echo Installing gcc version 9
+  echo
+  sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+  sudo apt-get update
+  sudo apt-get install -y gcc-9 g++-9
+}
+
+time {
+  update_gcc || sleep 10 || update_gcc || sleep 20 || update_gcc
+}
+export CXX=g++-9
+export CC=gcc-9
+
+bazel_install
+export PATH="$HOME/bin:$PATH"
+
+echo
+echo Tool versions
+echo
+$CC --version
+bazel --version
+
+
+echo
+echo Running Bazel build and test
+echo
+
 cd "${KOKORO_ARTIFACTS_DIR}/github/distbench"
+bazel build --cxxopt='-std=c++17' :all
 bazel test --cxxopt='-std=c++17' :all
+bazel shutdown
 
