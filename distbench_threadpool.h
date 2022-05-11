4f67b1f187c50a938d31ba5b44bb736575e50966
Olivier Serres <oserres@google.com>
2022-05-11 16:36:26 -0700

    distbench_threadpool: Add a simple threadpool implementation
    It is used to limit the number of threads created by
    protocol_driver_grpc_async_callback for processing RPC responses.
    
    It is a simple implementation meant to not add dependencies.

---
+++ distbench_threadpool.h
@@ -0,0 +1,42 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef DISTBENCH_DISTBENCH_THREADPOOL_H_
+#define DISTBENCH_DISTBENCH_THREADPOOL_H_
+
+#include <queue>
+#include <thread>
+#include <vector>
+
+#include "absl/synchronization/notification.h"
+#include "distbench_utils.h"
+
+namespace distbench {
+
+class DistbenchThreadpool {
+ public:
+  DistbenchThreadpool(int nb_threads);
+  ~DistbenchThreadpool();
+  void AddWork(std::function<void()> function);
+
+ private:
+  mutable absl::Mutex mutex_;
+  absl::Notification shutdown_;
+  std::vector<std::thread> threads_;
+  std::queue<std::function<void()> > work_queue_;
+};
+
+}  // namespace distbench
+
+#endif  // DISTBENCH_DISTBENCH_THREADPOOL_H_
---
+++ distbench_threadpool_test.cc
@@ -0,0 +1,52 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "distbench_threadpool.h"
+
+#include "gtest/gtest.h"
+#include "gtest_utils.h"
+
+namespace {
+
+TEST(DistBenchThreadPool, Constructor) {
+  distbench::DistbenchThreadpool dtp{4};
+};
+
+TEST(DistBenchThreadPool, PerformSimpleWork) {
+  std::atomic<int> work_counter = 0;
+  {
+    distbench::DistbenchThreadpool dtp{4};
+    for (int i = 0; i < 1000; i++) {
+      dtp.AddWork([&]() { ++work_counter; });
+    }
+  }  // Complete the work of dtp.
+  ASSERT_EQ(work_counter, 1000);
+};
+
+TEST(DistBenchThreadPool, ParallelAddTest) {
+  std::atomic<int> work_counter = 0;
+  {
+    distbench::DistbenchThreadpool dtp_work_performer{4};
+    {
+      distbench::DistbenchThreadpool dtp_work_generator{4};
+      for (int i = 0; i < 1000; i++) {
+        dtp_work_generator.AddWork(
+            [&]() { dtp_work_performer.AddWork([&]() { ++work_counter; }); });
+      }
+    }  // Complete the work of dtp_work_generator.
+  }    // Complete the work of dtp_work_performer.
+  ASSERT_EQ(work_counter, 1000);
+}
+
+}  // namespace
