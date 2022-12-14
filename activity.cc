3e9e0ea9c753dd3a9bf077e210e126b8e8b92604
Abhay Patil <abhpat@google.com>
2022-12-14 15:24:49 -0800

    FixFormatting: Ran make clang-format

--- activity.cc
+++ activity.cc
@@ -14,8 +14,8 @@
 
 #include "activity.h"
 
-#include "glog/logging.h"
 #include "boost/preprocessor/repetition/repeat.hpp"
+#include "glog/logging.h"
 
 namespace distbench {
 
@@ -141,42 +141,43 @@ ActivityLog PolluteDataCache::GetActivityLog() {
 // ...
 // ...
 // ...
-// DummyFunc_{POLLUTE_ICACHE_LOOP_SIZE-1}_0, ..., DummyFunc_{POLLUTE_ICACHE_LOOP_SIZE-1}_{POLLUTE_ICACHE_LOOP_SIZE-1}.
-// Each of these functions returns an int.
+// DummyFunc_{POLLUTE_ICACHE_LOOP_SIZE-1}_0, ...,
+// DummyFunc_{POLLUTE_ICACHE_LOOP_SIZE-1}_{POLLUTE_ICACHE_LOOP_SIZE-1}. Each of
+// these functions returns an int.
 //
 // We create this 2D array because BOOST_PP_REPEAT() macro can
 // generate at most 255 repetitions.
 #define POLLUTE_ICACHE_LOOP_SIZE 100
-#define GENERATE_FUNC_INNER_LOOP(z, n, prefix)  \
-  int DummyFunc_ ## prefix ## _ ## n (bool do_work) {       \
-    /*
-     * The purpose of 'do_work' part of the function is only to increase
-     * the size of the function.
-     */ \
-    if (do_work) {                \
-      int result = 0;             \
-      for (int i=0; i<n; i++) {   \
-        result += i*i;            \
-        result /= i+1;            \
-        result -= i/2;            \
-        result /= i*i;            \
-        result += result*i;       \
-        result /= result+1;       \
-        result -= result/2;       \
-        result *= result;         \
-      }                           \
-      return result;              \
-    } else                        \
-      return n;                   \
+#define GENERATE_FUNC_INNER_LOOP(z, n, prefix)                           \
+  int DummyFunc_##prefix##_##n(bool do_work) {                           \
+    /*                                                                   \
+     * The purpose of 'do_work' part of the function is only to increase \
+     * the size of the function.                                         \
+     */                                                                  \
+    if (do_work) {                                                       \
+      int result = 0;                                                    \
+      for (int i = 0; i < n; i++) {                                      \
+        result += i * i;                                                 \
+        result /= i + 1;                                                 \
+        result -= i / 2;                                                 \
+        result /= i * i;                                                 \
+        result += result * i;                                            \
+        result /= result + 1;                                            \
+        result -= result / 2;                                            \
+        result *= result;                                                \
+      }                                                                  \
+      return result;                                                     \
+    } else                                                               \
+      return n;                                                          \
   }
 #define GENERATE_FUNC_OUTER_LOOP(z, n, text) \
   BOOST_PP_REPEAT(POLLUTE_ICACHE_LOOP_SIZE, GENERATE_FUNC_INNER_LOOP, n)
 
 // Get the pointers to the functions created above and store it in
 // func_ptr_array.
-#define GET_FUNC_PTR_INNER_LOOP(z, n, prefix) \
+#define GET_FUNC_PTR_INNER_LOOP(z, n, prefix)                \
   func_ptr_array_[(prefix * POLLUTE_ICACHE_LOOP_SIZE + n)] = \
-    &DummyFunc_ ## prefix ## _ ## n;
+      &DummyFunc_##prefix##_##n;
 #define GET_FUNC_PTR_OUTER_LOOP(z, n, text) \
   BOOST_PP_REPEAT(POLLUTE_ICACHE_LOOP_SIZE, GET_FUNC_PTR_INNER_LOOP, n)
 
@@ -195,8 +196,8 @@ void PolluteInstructionCache::Initialize(ParsedActivityConfig* config) {
   std::random_device rd;
   mersenne_twister_prng_ = std::mt19937(rd());
 
-  function_invocations_per_iteration_ =
-      config->pollute_instruction_cache_config.function_invocations_per_iteration;
+  function_invocations_per_iteration_ = config->pollute_instruction_cache_config
+                                            .function_invocations_per_iteration;
 
   random_index_ = std::uniform_int_distribution<>(0, func_array_size - 1);
 
--- distbench_engine.cc
+++ distbench_engine.cc
@@ -449,7 +449,7 @@ absl::Status DistBenchEngine::Initialize(
   builder.AddChannelArgument(GRPC_ARG_ALLOW_REUSEPORT, 0);
   builder.RegisterService(this);
   server_ = builder.BuildAndStart();
-  server_address = GetBindAddressFromPort(*port); // port may have changed
+  server_address = GetBindAddressFromPort(*port);  // port may have changed
   if (!server_) {
     LOG(ERROR) << engine_name_ << ": Engine start failed on " << server_address;
     return absl::UnknownError("Engine service failed to start");
