747d80158a5ea21ea60b9d18a23986c1f451076e
Abhay Patil <abhpat@google.com>
2023-01-09 11:45:10 -0800

    Randomization: Initial Commit

---
+++ randomization/distribution_sample_generator.cc
@@ -0,0 +1,196 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "randomization/distribution_sample_generator.h"
+
+namespace distbench {
+
+absl::Status ValidatePmfConfig(const DistributionConfig& config) {
+  float cdf = 0;
+  int num_variables = -1;
+  for (const auto& point : config.pmf_points()) {
+    if (point.data_points().empty()) {
+      return absl::InvalidArgumentError(
+          absl::StrCat("The size of data_points cannot be 0."));
+    }
+    if (num_variables == -1) {
+      num_variables = point.data_points_size();
+    } else {
+      if (num_variables != point.data_points_size())
+        return absl::InvalidArgumentError(absl::StrCat(
+            "The size of data_points must be same in all PmfPoints."));
+    }
+    cdf += point.pmf();
+  }
+  if (cdf != 1) {
+    return absl::InvalidArgumentError(
+        absl::StrCat("Cumulative value of all PMFs should be 1. It is '", cdf,
+                     "' instead."));
+  }
+  return absl::OkStatus();
+};
+
+absl::Status ValidateCdfConfig(const DistributionConfig& config) {
+  if (config.cdf_points_size() == 0) {
+    return absl::InvalidArgumentError(
+        absl::StrCat("CDF is not provided for '", config.name(), "'."));
+  }
+
+  auto prev_cdf = config.cdf_points(0).cdf();
+  if (prev_cdf < 0) {
+    return absl::InvalidArgumentError(
+        absl::StrCat("The cdf value:'", prev_cdf,
+                     "' must not be negative in CDF:'", config.name(), "'."));
+  }
+
+  auto prev_value = config.cdf_points(0).value();
+  for (int i = 1; i < config.cdf_points_size(); i++) {
+    auto curr_value = config.cdf_points(i).value();
+    auto curr_cdf = config.cdf_points(i).cdf();
+    if (curr_value <= prev_value) {
+      return absl::InvalidArgumentError(absl::StrCat(
+          "The value:'", curr_value, "' must be greater than previous_value:'",
+          prev_value, "' at index '", i, "' in CDF:'", config.name(), "'."));
+    }
+    if (curr_cdf < prev_cdf) {
+      return absl::InvalidArgumentError(
+          absl::StrCat("The cdf value:'", curr_cdf,
+                       "' must be greater than previous cdf value:'", prev_cdf,
+                       "' at index '", i, "' in CDF:'", config.name(), "'."));
+    }
+    prev_value = curr_value;
+    prev_cdf = curr_cdf;
+  }
+
+  auto last_configured_cdf =
+      config.cdf_points(config.cdf_points_size() - 1).cdf();
+  if (last_configured_cdf != 1) {
+    return absl::InvalidArgumentError(absl::StrCat(
+        "The maximum value of cdf is '", last_configured_cdf, "' in CDF:'",
+        config.name(), "'. It must be exactly equal to 1."));
+  }
+  return absl::OkStatus();
+};
+
+absl::Status ValidateDistributionConfig(const DistributionConfig& config) {
+  auto cdf_present = config.cdf_points_size() != 0;
+  auto pmf_present = config.pmf_points_size() != 0;
+
+  if (cdf_present == pmf_present) {
+    return absl::InvalidArgumentError(
+        absl::StrCat("Exactly one of CDF and PMF must be provided for '",
+                     config.name(), "'."));
+  }
+
+  if (cdf_present) return ValidateCdfConfig(config);
+  if (pmf_present) return ValidatePmfConfig(config);
+
+  return absl::InvalidArgumentError(
+      absl::StrCat("Review CDF and PMF for '", config.name(), "'."));
+};
+
+absl::StatusOr<std::unique_ptr<DistributionSampleGenerator>>
+AllocateSampleGenerator(const DistributionConfig& config) {
+  std::unique_ptr<DistributionSampleGenerator> sample_gen_lib;
+  sample_gen_lib = std::make_unique<DistributionSampleGenerator>();
+  auto status = sample_gen_lib->Initialize(config);
+  if (!status.ok()) return status;
+  return sample_gen_lib;
+};
+
+absl::Status DistributionSampleGenerator::InitializeWithCdf(
+    const DistributionConfig& config) {
+  auto status = ValidateCdfConfig(config);
+  if (!status.ok()) return status;
+
+  DistributionConfig config_with_pmf;
+  config_with_pmf.set_name(config.name());
+
+  auto* pmf_point = config_with_pmf.add_pmf_points();
+  pmf_point->set_pmf(config.cdf_points(0).cdf());
+  auto* data_point = pmf_point->add_data_points();
+  data_point->set_exact(config.cdf_points(0).value());
+
+  auto prev_cdf = config.cdf_points(0).cdf();
+  for (int i = 1; i < config.cdf_points_size(); i++) {
+    auto curr_cdf = config.cdf_points(i).cdf();
+    auto* pmf_point = config_with_pmf.add_pmf_points();
+    pmf_point->set_pmf(curr_cdf - prev_cdf);
+
+    auto* data_point = pmf_point->add_data_points();
+    data_point->set_exact(config.cdf_points(i).value());
+
+    prev_cdf = curr_cdf;
+  }
+
+  return InitializeWithPmf(config_with_pmf);
+};
+
+absl::Status DistributionSampleGenerator::InitializeWithPmf(
+    const DistributionConfig& config) {
+  auto status = ValidatePmfConfig(config);
+  if (!status.ok()) return status;
+
+  unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+  generator_ = std::default_random_engine(seed);
+  std::vector<float> pmf;
+  num_dimensions_ = config.pmf_points(0).data_points_size();
+  exact_value_.resize(num_dimensions_);
+  range_.resize(num_dimensions_);
+  is_exact_value_.resize(num_dimensions_);
+  for (const auto& point : config.pmf_points()) {
+    for (int i = 0; i < point.data_points_size(); i++) {
+      if (point.data_points(i).has_exact()) {
+        exact_value_[i].push_back(point.data_points(i).exact());
+        range_[i].push_back(std::uniform_int_distribution<>());
+        is_exact_value_[i].push_back(true);
+      } else {
+        exact_value_[i].push_back(0);
+        range_[i].push_back(std::uniform_int_distribution<>(
+            point.data_points(i).lower(), point.data_points(i).upper()));
+        is_exact_value_[i].push_back(false);
+      }
+    }
+    pmf.push_back(point.pmf());
+  }
+
+  distribution_array_ = std::discrete_distribution<int>(pmf.begin(), pmf.end());
+  return absl::OkStatus();
+};
+
+absl::Status DistributionSampleGenerator::Initialize(
+    const DistributionConfig& config) {
+  std::random_device rd;
+  mersenne_twister_prng_ = std::mt19937(rd());
+  if (config.cdf_points_size()) return InitializeWithCdf(config);
+  if (config.pmf_points_size()) return InitializeWithPmf(config);
+  return absl::InvalidArgumentError(
+      absl::StrCat("Add CDF or PMF to '", config.name(), "'."));
+};
+
+std::vector<int> DistributionSampleGenerator::GetRandomSample() {
+  auto index = distribution_array_(generator_);
+  std::vector<int> sample;
+
+  for (int dim = 0; dim < num_dimensions_; dim++) {
+    if (is_exact_value_[dim][index]) {
+      sample.push_back(exact_value_[dim][index]);
+    } else {
+      sample.push_back(range_[dim][index](mersenne_twister_prng_));
+    }
+  }
+  return sample;
+};
+
+}  // namespace distbench
---
+++ randomization/distribution_sample_generator_test.cc
@@ -0,0 +1,490 @@
+// Copyright 2022 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "randomization/distribution_sample_generator.h"
+
+#include "gtest/gtest.h"
+
+namespace distbench {
+
+class DistributionSampleGeneratorTest : public ::testing::Test {};
+
+TEST(DistributionSampleGeneratorTest, NoDistributionConfig) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  auto status = ValidateDistributionConfig(config);
+  ASSERT_EQ(
+      status,
+      absl::InvalidArgumentError(
+          "Exactly one of CDF and PMF must be provided for 'MyReqPayloadDC'."));
+}
+
+TEST(DistributionSampleGeneratorTest, BothCdfAndPdfConfig) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+
+  auto* pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(1);
+  auto* data_point = pmf_point->add_data_points();
+  data_point->set_exact(10);
+
+  auto* cdf_point = config.add_cdf_points();
+  cdf_point->set_cdf(1);
+  cdf_point->set_value(10);
+
+  auto status = ValidateDistributionConfig(config);
+  ASSERT_EQ(
+      status,
+      absl::InvalidArgumentError(
+          "Exactly one of CDF and PMF must be provided for 'MyReqPayloadDC'."));
+}
+
+TEST(DistributionSampleGeneratorTest, ValidateDistributionPmfConfig) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  for (float i = 1; i < 5; i++) {
+    auto* pmf_point = config.add_pmf_points();
+    pmf_point->set_pmf(i / 10);
+    auto* data_point = pmf_point->add_data_points();
+    data_point->set_exact(i);
+  }
+  auto status = ValidateDistributionConfig(config);
+  ASSERT_EQ(status, absl::OkStatus());
+}
+
+TEST(DistributionSampleGeneratorTest, InvalidDistributionPmfConfig) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  for (float i = 1; i < 5; i++) {
+    auto* pmf_point = config.add_pmf_points();
+    pmf_point->set_pmf(i / 20);
+    auto* data_point = pmf_point->add_data_points();
+    data_point->set_exact(i);
+  }
+  auto status = ValidateDistributionConfig(config);
+  ASSERT_EQ(
+      status,
+      absl::InvalidArgumentError(
+          "Cumulative value of all PMFs should be 1. It is '0.5' instead."));
+}
+
+TEST(DistributionSampleGeneratorTest, ValidateDistributionCdfConfig) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  float cdf = 0;
+  for (float i = 1; i < 5; i++) {
+    auto* cdf_point = config.add_cdf_points();
+    cdf_point->set_value(i);
+    cdf += i / 10;
+    cdf_point->set_cdf(cdf);
+  }
+  auto status = ValidateDistributionConfig(config);
+  ASSERT_EQ(status, absl::OkStatus());
+}
+
+TEST(DistributionSampleGeneratorTest,
+     InvalidDistributionCdfConfigErraneousCdf) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  float cdf = 0;
+  for (float i = 1; i < 5; i++) {
+    auto* cdf_point = config.add_cdf_points();
+    cdf_point->set_value(i);
+    cdf += i / 20;
+    cdf_point->set_cdf(cdf);
+  }
+  auto status = ValidateDistributionConfig(config);
+  ASSERT_EQ(status,
+            absl::InvalidArgumentError(
+                "The maximum value of cdf is '0.5' in CDF:'MyReqPayloadDC'. "
+                "It must be exactly equal to 1."));
+}
+
+TEST(DistributionSampleGeneratorTest,
+     InvalidDistributionCdfConfigNonIncreasingValues) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  float cdf = 0;
+  for (float i = 1; i < 5; i++) {
+    auto* cdf_point = config.add_cdf_points();
+    cdf_point->set_value(100 - 10 * i);
+    cdf += i / 20;
+    cdf_point->set_cdf(cdf);
+  }
+  auto status = ValidateDistributionConfig(config);
+  ASSERT_EQ(status,
+            absl::InvalidArgumentError(
+                "The value:'80' must be greater than previous_value:'90' at "
+                "index '1' in CDF:'MyReqPayloadDC'."));
+}
+
+TEST(DistributionSampleGeneratorTest,
+     InvalidDistributionCdfConfigNonIncreasingCdf) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  float cdf = 0;
+  for (float i = 1; i < 5; i++) {
+    auto* cdf_point = config.add_cdf_points();
+    cdf_point->set_value(i);
+    cdf += i / 20;
+    cdf_point->set_cdf(1 / cdf);
+  }
+  auto status = ValidateDistributionConfig(config);
+  ASSERT_EQ(status,
+            absl::InvalidArgumentError(
+                "The cdf value:'6.66667' must be greater than previous cdf "
+                "value:'20' at index '1' in CDF:'MyReqPayloadDC'."));
+}
+
+TEST(DistributionSampleGeneratorTest, FullTestPmf) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  for (float i = 1; i < 5; i++) {
+    // Generate values of 1, 2, 3 and 4 with a probability
+    // of 0.1, 0.2, 0.3 and 0.4 respectively.
+    auto* pmf_point = config.add_pmf_points();
+    pmf_point->set_pmf(i / 10);
+    auto* data_point = pmf_point->add_data_points();
+    data_point->set_exact(i);
+  }
+  auto status = ValidateDistributionConfig(config);
+  ASSERT_EQ(status, absl::OkStatus());
+
+  auto maybe_sg = AllocateSampleGenerator(config);
+  ASSERT_EQ(maybe_sg.ok(), true);
+  auto sg = std::move(maybe_sg.value());
+
+  std::map<int, int> sample_count;
+  for (int i = 0; i < 5000; i++) {
+    auto sample_value = sg->GetRandomSample();
+    sample_count[sample_value[0]]++;
+  }
+
+  ASSERT_LT(sample_count[1], sample_count[2]);
+  ASSERT_LT(sample_count[2], sample_count[3]);
+  ASSERT_LT(sample_count[3], sample_count[4]);
+}
+
+TEST(DistributionSampleGeneratorTest, FullTestCdf) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  float cdf = 0;
+  for (float i = 1; i < 5; i++) {
+    // Generate values of 1, 2, 3 and 4 with a CDF
+    // of 0.1, 0.3, 0.6 and 1.0 respectively.
+    cdf += i / 10;
+    auto* cdf_point = config.add_cdf_points();
+    cdf_point->set_value(i);
+    cdf_point->set_cdf(cdf);
+  }
+
+  auto maybe_sg = AllocateSampleGenerator(config);
+  ASSERT_EQ(maybe_sg.status(), absl::OkStatus());
+  auto sg = std::move(maybe_sg.value());
+
+  std::map<int, int> sample_count;
+  for (int i = 0; i < 5000; i++) {
+    auto sample_value = sg->GetRandomSample();
+    sample_count[sample_value[0]]++;
+  }
+
+  ASSERT_LT(sample_count[1], sample_count[2]);
+  ASSERT_LT(sample_count[2], sample_count[3]);
+  ASSERT_LT(sample_count[3], sample_count[4]);
+}
+
+TEST(DistributionSampleGeneratorTest, InvalidCdfInitializeTest) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  float cdf = 0;
+  for (float i = 1; i < 5; i++) {
+    // Generate values of 1, 2, 3 and 4 with a CDF
+    // of 0.01, 0.03, 0.06 and 0.1 respectively.
+    cdf += i / 100;
+    auto* cdf_point = config.add_cdf_points();
+    cdf_point->set_value(i);
+    cdf_point->set_cdf(cdf);
+  }
+
+  auto maybe_sg = AllocateSampleGenerator(config);
+  ASSERT_EQ(
+      maybe_sg.status(),
+      absl::InvalidArgumentError(
+          "The maximum value of cdf is '0.1' in CDF:'MyReqPayloadDC'. It must "
+          "be exactly equal to 1."));
+}
+
+TEST(DistributionSampleGeneratorTest, InvalidPmfInitializeTest) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  for (float i = 1; i < 5; i++) {
+    // Generate values of 1, 2, 3 and 4 with a probability
+    // of 0.01, 0.02, 0.03 and 0.04 respectively.
+    auto* pmf_point = config.add_pmf_points();
+    pmf_point->set_pmf(i / 100);
+    auto* data_point = pmf_point->add_data_points();
+    data_point->set_exact(i);
+  }
+
+  auto maybe_sg = AllocateSampleGenerator(config);
+  ASSERT_EQ(
+      maybe_sg.status(),
+      absl::InvalidArgumentError(
+          "Cumulative value of all PMFs should be 1. It is '0.1' instead."));
+}
+
+TEST(DistributionSampleGeneratorTest, PmfRangeTest) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+
+  // The range of 'small' numbers.
+  auto* pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(0.2);
+  auto* data_point = pmf_point->add_data_points();
+  data_point->set_lower(10);
+  data_point->set_upper(20);
+
+  // The range of 'big' numbers.
+  pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(0.8);
+  data_point = pmf_point->add_data_points();
+  data_point->set_lower(10000);
+  data_point->set_upper(10010);
+
+  auto maybe_sg = AllocateSampleGenerator(config);
+  ASSERT_EQ(maybe_sg.ok(), true);
+  auto sg = std::move(maybe_sg.value());
+
+  int small_count = 0;
+  int big_count = 0;
+  for (int i = 0; i < 5000; i++) {
+    auto sample = sg->GetRandomSample();
+    auto sample_value = sample[0];
+    if ((sample_value >= 10) && (sample_value <= 20))
+      small_count++;
+    else if ((sample_value >= 10000) && (sample_value <= 10010))
+      big_count++;
+    else {
+      FAIL() << "Out of range value '" << sample_value
+             << "' generated by sample generator: '" << config.name() << "'.";
+    }
+  }
+  ASSERT_LT(small_count, big_count);
+}
+
+TEST(DistributionSampleGeneratorTest, PmfRangeAndValueMixTest) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+
+  // 10 is our 'small' number.
+  auto* pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(0.2);
+  auto* data_point = pmf_point->add_data_points();
+  data_point->set_exact(10);
+
+  // [10000, 10010] range of 'big' numbers.
+  pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(0.8);
+  data_point = pmf_point->add_data_points();
+  data_point->set_lower(10000);
+  data_point->set_upper(10010);
+
+  auto maybe_sg = AllocateSampleGenerator(config);
+  ASSERT_EQ(maybe_sg.ok(), true);
+  auto sg = std::move(maybe_sg.value());
+
+  int small_count = 0;
+  int big_count = 0;
+  for (int i = 0; i < 5000; i++) {
+    auto sample = sg->GetRandomSample();
+    auto sample_value = sample[0];
+    if (sample_value == 10)
+      small_count++;
+    else if ((sample_value >= 10000) && (sample_value <= 10010))
+      big_count++;
+    else {
+      FAIL() << "Out of range value '" << sample_value
+             << "' generated by sample generator: '" << config.name() << "'.";
+    }
+  }
+  ASSERT_LT(small_count, big_count);
+}
+
+TEST(DistributionSampleGeneratorTest, Pmf2Vars) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+  for (float i = 1; i < 5; i++) {
+    // Generate values of (1,10) , (2,20), (3,30) and (4,40) with a probability
+    // of 0.1, 0.2, 0.3 and 0.4 respectively.
+    auto* pmf_point = config.add_pmf_points();
+    pmf_point->set_pmf(i / 10);
+
+    auto* data_point = pmf_point->add_data_points();
+    data_point->set_exact(i);
+
+    data_point = pmf_point->add_data_points();
+    data_point->set_exact(i * 10);
+  }
+  auto status = ValidateDistributionConfig(config);
+  ASSERT_EQ(status, absl::OkStatus());
+
+  auto maybe_sg = AllocateSampleGenerator(config);
+  ASSERT_EQ(maybe_sg.ok(), true);
+  auto sg = std::move(maybe_sg.value());
+
+  std::map<std::vector<int>, int> sample_count;
+  for (int i = 0; i < 5000; i++) {
+    auto sample = sg->GetRandomSample();
+    ASSERT_EQ(sample.size(), 2);
+    sample_count[sample]++;
+  }
+
+  ASSERT_LT(sample_count[std::vector({1, 10})],
+            sample_count[std::vector({2, 20})]);
+  ASSERT_LT(sample_count[std::vector({2, 20})],
+            sample_count[std::vector({3, 30})]);
+  ASSERT_LT(sample_count[std::vector({3, 30})],
+            sample_count[std::vector({4, 40})]);
+}
+
+TEST(DistributionSampleGeneratorTest, PmfRangeAndValueMixTest2Vars) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+
+  // The 'small' sample has first variable exactly equal to 10.
+  // The 'small' sample has second variable exactly in range [20, 30].
+  auto* pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(0.2);
+  auto* data_point = pmf_point->add_data_points();
+  data_point->set_exact(10);
+  data_point = pmf_point->add_data_points();
+  data_point->set_lower(20);
+  data_point->set_upper(30);
+
+  // The 'big' sample has first variable exactly in range [10010, 10030].
+  // The 'big' sample has second variable exactly equal to 10000.
+  pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(0.8);
+  data_point = pmf_point->add_data_points();
+  data_point->set_lower(10010);
+  data_point->set_upper(10030);
+  data_point = pmf_point->add_data_points();
+  data_point->set_exact(10000);
+
+  auto maybe_sg = AllocateSampleGenerator(config);
+  ASSERT_EQ(maybe_sg.ok(), true);
+  auto sg = std::move(maybe_sg.value());
+
+  int small_count = 0;
+  int big_count = 0;
+  for (int i = 0; i < 5000; i++) {
+    auto sample = sg->GetRandomSample();
+    ASSERT_EQ(sample.size(), 2);
+    auto var1 = sample[0];
+    auto var2 = sample[1];
+    if ((var1 == 10) && (var2 >= 20 && var2 <= 30))
+      small_count++;
+    else if ((var1 >= 10010 && var1 <= 10030) && (var2 == 10000))
+      big_count++;
+    else {
+      FAIL() << "Out of range value (" << var1 << "," << var2
+             << ") generated by sample generator: '" << config.name() << "'.";
+    }
+  }
+  ASSERT_LT(small_count, big_count);
+}
+
+TEST(DistributionSampleGeneratorTest, Pmf3Vars) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+
+  auto* pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(0.1);
+  auto* data_point = pmf_point->add_data_points();
+  data_point->set_exact(10);
+  data_point = pmf_point->add_data_points();
+  data_point->set_exact(100);
+  data_point = pmf_point->add_data_points();
+  data_point->set_exact(1000);
+
+  pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(0.3);
+  data_point = pmf_point->add_data_points();
+  data_point->set_exact(30);
+  data_point = pmf_point->add_data_points();
+  data_point->set_exact(300);
+  data_point = pmf_point->add_data_points();
+  data_point->set_exact(3000);
+
+  pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(0.6);
+  data_point = pmf_point->add_data_points();
+  data_point->set_exact(60);
+  data_point = pmf_point->add_data_points();
+  data_point->set_exact(600);
+  data_point = pmf_point->add_data_points();
+  data_point->set_exact(6000);
+
+  auto maybe_sg = AllocateSampleGenerator(config);
+  ASSERT_EQ(maybe_sg.ok(), true);
+  auto sg = std::move(maybe_sg.value());
+
+  int small_count = 0;
+  int medium_count = 0;
+  int big_count = 0;
+  for (int i = 0; i < 5000; i++) {
+    auto sample = sg->GetRandomSample();
+    ASSERT_EQ(sample.size(), 3);
+    auto var0 = sample[0];
+    auto var1 = sample[1];
+    auto var2 = sample[2];
+    if ((var0 == 10) && (var1 == 100) && (var2 == 1000))
+      small_count++;
+    else if ((var0 == 30) && (var1 == 300) && (var2 == 3000))
+      medium_count++;
+    else if ((var0 == 60) && (var1 == 600) && (var2 == 6000))
+      big_count++;
+    else {
+      FAIL() << "Out of range value (" << var0 << "," << var1 << "," << var2
+             << ") generated by sample generator: '" << config.name() << "'.";
+    }
+  }
+
+  ASSERT_LT(small_count, medium_count);
+  ASSERT_LT(medium_count, big_count);
+}
+
+TEST(DistributionSampleGeneratorTest, PmfPointDifferentVariableNumbers) {
+  DistributionConfig config;
+  config.set_name("MyReqPayloadDC");
+
+  auto* pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(0.1);
+  auto* data_point = pmf_point->add_data_points();
+  data_point->set_exact(10);
+  data_point = pmf_point->add_data_points();
+  data_point->set_exact(100);
+
+  pmf_point = config.add_pmf_points();
+  pmf_point->set_pmf(0.9);
+  data_point = pmf_point->add_data_points();
+  data_point->set_exact(90);
+
+  auto maybe_sg = AllocateSampleGenerator(config);
+  ASSERT_EQ(maybe_sg.status(),
+            absl::InvalidArgumentError(
+                "The size of data_points must be same in all PmfPoints."));
+}
+
+}  // namespace distbench
