07bebc2f1f88b2a1eab29f6732eceddd9ef4fb39
Dan Manjarres <danmanj@google.com>
2023-09-07 10:01:03 -0700

    test_builder: allow the values of keys to come from files.

--- test_builder/test_builder_test.sh
+++ test_builder/test_builder_test.sh
@@ -7,6 +7,7 @@ function compare_golden_file() {
   # or
   # produced by ./test_builder -o - ...
   local invocation="$(head -n 1 "$1" | cut -f 7- "-d ")"
+  read invocation <<< $invocation  # un-escape the string
   # Invoke the current code with the same arguments and compare its output to
   # the reference output:
   diff -u -I '^# produced by ' "$1" <(../test_builder -o - ${invocation})
@@ -17,12 +18,12 @@ which dist_to_proto
 test_builder/test_builder -h
 
 cd "./test_builder/test_builder_golden_configs"
-for config in *; do
+for config in *.config; do
   echo checking ${config}
   ../../distbench check_test --infile $PWD/${config}
 done
 
-for f in *; do
+for f in *.config; do
   echo Comparing against "$f":
   if compare_golden_file "$f"; then
     echo "Output matches! :-)"
